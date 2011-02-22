e421cf2b7883428287f55921be8719dc43f32b5c
Nathan Hamblen <nathan@technically.us>
2011-02-22 01:49:15 -0500

    script with optional name

--- src/main/scala/apply.scala
+++ src/main/scala/apply.scala
@@ -4,15 +4,23 @@ import scala.util.control.Exception.catching
 import java.io.File
 
 object Apply {
-  def config(user: String, repo: String, launch: String, props: Option[String]) = {
+  def config(user: String, repo: String, launch: String, props: Map[String,String]) = {
     val launchconfig = configdir(/(user, repo))
-    write(launchconfig, launch + boot).toLeft {
-      "Wrote " + launchconfig
+
+    val name = props.getOrElse("executable", repo)
+    val place = homedir(/("bin", name))
+    val options = props.getOrElse("options", "")
+    write(launchconfig, launch + boot).orElse {
+      write(place, script(options, launchconfig))
+    }.toLeft {
+      "Conscripted %s/%s to %s".format(user, repo, place)
     }
   }
   def / (a: String, b: String) = a + File.separatorChar + b
   def configdir(path: String) =
-    new File(System.getProperty("user.home"), /(".conscript", path))
+    homedir(/(".conscript", path))
+  def homedir(path: String) =
+    new File(System.getProperty("user.home"), path)
   def write(file: File, body: String) =
     mkdir(file) orElse {
       catching(classOf[java.io.IOException]).either {
@@ -26,9 +34,11 @@ object Apply {
     catching(classOf[SecurityException]).either {
       new File(file.getParent).mkdirs()
     }.left.toOption.map { e => "Unable to create path " + file }
-  def script(options: String) = 
+  def script(options: String, launchconfig: File) = 
     """#!/bin/sh
-      |java -jar ~/.conscript/sbt-launch.jar %s "$@"""".stripMargin.format(options)
+      |java -jar %s %s @%s "$@"
+      |""" .stripMargin.
+        format(configdir("sbt-launch.jar"), options, launchconfig.getCanonicalPath)
   val boot = """
             |[boot]
             |  directory: %s
--- src/main/scala/github.scala
+++ src/main/scala/github.scala
@@ -8,12 +8,22 @@ object Github {
   def lookup(user: String, repo: String) = {
     allCatch.opt { http(gh / "blob" / "all" / user / repo / "master" ># (
       'blobs ? obj ~> { blobs =>
-        def source(file: String) =
+        def source[T](file: String) =
           blobs.flatMap(Symbol("src/main/conscript/" + file) ? str).headOption.map {
-            sha => http(gh / "blob" / "show" / user / repo / sha as_str)
+            sha => gh / "blob" / "show" / user / repo / sha
           }
-        source("launchconfig").map {
-          lc => (lc, source("default.properties"))
+        def propmap(req: Request) =
+          http(req >> { stm =>
+            import scala.collection.JavaConversions._
+            val p = new java.util.Properties
+            p.load(stm)
+            (Map.empty[String, String] /: p.propertyNames) { (m, k) =>
+              m + (k.toString -> p.getProperty(k.toString))
+            }
+          })
+        source("launchconfig").map { req =>
+          val lc = http(req as_str)
+          (lc, source("default.properties").map(propmap).getOrElse { Map.empty })
         }
       }
     )) }.flatMap { o => o }.toRight {
