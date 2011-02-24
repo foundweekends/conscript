04936b32b47c2a738963ba4a52547b2e0b277943
Nathan Hamblen <nathan@technically.us>
2011-02-24 01:45:14 -0500

    script and vers updates

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@ echo "
 Fetching current launch configuration...
 "
 CS=$HOME/.conscript
-CLC=$CS/n8han/conscript
-mkdir -p $HOME/.conscript/n8han
-curl https://github.com/n8han/conscript/raw/master/src/main/conscript/launchconfig \
+CSCS=$CS/n8han/conscript
+CLC=$CSCS/launchconfig
+mkdir -p $CSCS
+curl https://github.com/n8han/conscript/raw/master/src/main/conscript/cs/launchconfig \
     > $CLC
 echo "
 [boot]
@@ -19,12 +20,15 @@ java -jar $CS/sbt-launch.jar  @$CLC \"\$@\"" > $HOME/bin/cs
 
 chmod a+x $HOME/bin/cs
 
-echo "
+LJ=sbt-launch-0.7.4.jar
+if [ ! -f $CS/$LJ ]; then
+    echo "
 Fetching launcher...
 "
-
-curl "https://simple-build-tool.googlecode.com/files/sbt-launch-0.7.4.jar" \
-    > $CS/sbt-launch.jar
+    curl "https://simple-build-tool.googlecode.com/files/$LJ" \
+        > $CS/$LJ
+    ln -sf $CS/$LJ $CS/sbt-launch.jar
+fi
 
 echo "
 conscript installed to $HOME/bin/cs
