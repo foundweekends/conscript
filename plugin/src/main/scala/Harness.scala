0f9dc12d6c899a04aadbdd85c71a7bbccfb3b638
Nathan Hamblen <nathan@technically.us>
2011-03-15 02:20:10 -0400

    include dependency in harness

--- plugin/src/main/scala/Harness.scala
+++ plugin/src/main/scala/Harness.scala
@@ -3,6 +3,9 @@ package conscript
 import sbt.{FileUtilities => U,Path}
 
 trait Harness { self: sbt.DefaultProject =>
+  val launchInterface = 
+    "org.scala-tools.sbt" % "launcher-interface" % "0.7.4" % "provided"
+
   def conscriptBase = (mainSourcePath / "conscript") ##
   def conscriptOutput = outputPath / "conscript"
   def conscriptBoot = conscriptOutput / "boot"
