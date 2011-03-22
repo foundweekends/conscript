7a05aa0573ee0cb06e7a3a4af4799f3c818f0e78
Nathan Hamblen <nathan@technically.us>
2011-03-22 01:04:15 -0400

    use harness, update deps

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,13 +1,14 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends ParentProject(info) 
-    with posterous.Publish {
+    with posterous.Publish 
+    with conscript.Harness {
   lazy val app = project("app", 
                          "conscript", 
                          new DefaultProject(_) with sxr.Publish {
     val launch = "org.scala-tools.sbt" % "launcher-interface" % "0.7.4" % "provided"
-    val dj = "net.databinder" %% "dispatch-lift-json" % "0.8.0.Beta3"
-    val dn = "net.databinder" %% "dispatch-http" % "0.8.0.Beta3"
+    val dj = "net.databinder" %% "dispatch-lift-json" % "0.8.0.Beta4"
+    val dn = "net.databinder" %% "dispatch-http" % "0.8.0.Beta4"
   })
   lazy val plugins = project("plugin", "conscript plugin", new PluginProject(_))
   override def postTitle(vers: String) = "conscript %s".format(vers)
--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -5,5 +5,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
   val posterous = "net.databinder" % "posterous-sbt" % "0.1.5"
   // publish source to sourced.implicit.ly
-  val sxr_publish = "net.databinder" % "sxr-publish" % "0.1.9"
+  val sxr_publish = "net.databinder" % "sxr-publish" % "0.2.0"
+  val conscript = "net.databinder" % "conscript-plugin" % "0.2.2-SNAPSHOT"
 }
