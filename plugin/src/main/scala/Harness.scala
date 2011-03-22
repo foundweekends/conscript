e84ca277b93a92741b4ab27f23a4ce8c2993653b
Nathan Hamblen <nathan@technically.us>
2011-03-21 23:24:38 -0400

    don't require DefaultScalaProject

--- plugin/src/main/scala/Harness.scala
+++ plugin/src/main/scala/Harness.scala
@@ -2,11 +2,12 @@ package conscript
 
 import sbt.{FileUtilities => U,Path}
 
-trait Harness { self: sbt.DefaultProject =>
+trait Harness { self: sbt.BasicDependencyProject =>
   val launchInterface = 
     "org.scala-tools.sbt" % "launcher-interface" % "0.7.4" % "provided"
 
-  def conscriptBase = (mainSourcePath / "conscript") ##
+  // can not use mainSourcePath, ParentProject does not implement
+  def conscriptBase = (path("src") / "main" / "conscript") ##
   def conscriptOutput = outputPath / "conscript"
   def conscriptBoot = conscriptOutput / "boot"
   def conscriptConfigs = conscriptOutput ** "launchconfig"
