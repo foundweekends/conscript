458848872ed7564e5c0474384a2914ed53c575ee
Nathan Hamblen <nathan@technically.us>
2012-01-21 17:38:04 -0500

    git dependency on dlj

--- project/build.scala
+++ project/build.scala
@@ -4,5 +4,5 @@ object Appy extends Build
   lazy val root =
     Project("conscript", file(".")) dependsOn(reboot)
   lazy val reboot =
-    uri("../dispatch-lift-json")
+    uri("git://github.com/dispatch/dispatch-lift-json.git#e5d565e9e07d0b8935ae9c3fb90cf57fd9c893aa")
 }
