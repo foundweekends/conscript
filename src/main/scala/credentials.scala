a002df9f0c7863ec6a761c62d8f8a02eb0753fb6
Nathan Hamblen <nathan@technically.us>
2012-04-26 08:38:32 -0400

    rm name/pass auth since we never used it

--- src/main/scala/credentials.scala
+++ src/main/scala/credentials.scala
@@ -7,29 +7,12 @@ trait Credentials {
   import scala.util.control.Exception.allCatch
   
   def withCredentials(req: RequestBuilder) =
-    (oauth map { case token => req.addHeader("Authorization", "token %s".format(token)) }).orElse(
-      credentials map { case (user, pass) => req as (user, pass) }) getOrElse req
+    (oauth map { 
+      case token => req.addHeader("Authorization", "token %s".format(token))
+    }).getOrElse { req }
   
   def oauth: Option[String] =
     Config.get("gh.oauth")
-  
-  lazy val credentials: Option[(String, String)] =
-    gitConfig("github.user") flatMap { user =>
-      gitConfig("github.password") map { password =>
-        (user, password)
-      }    
-    }
-  
-  // https://github.com/defunkt/gist/blob/master/lib/gist.rb#L237
-  def gitConfig(key: String): Option[String] =
-    allCatch opt {
-      Option(System.getenv(key.toUpperCase.replaceAll("""\.""", "_"))) map { Some(_) } getOrElse {
-        val gitExec = windows map {_ => "git.exe"} getOrElse {"git"}
-        val p = new java.lang.ProcessBuilder(gitExec, "config", "--global", key).start()
-        val reader = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream))
-        Option(reader.readLine)
-      }
-    } getOrElse {None}
   
   def windows =
     System.getProperty("os.name") match {
