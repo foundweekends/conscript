9dbea75fc44b5b5e5f8f8748788c4fc8b3eb5ac1
Nathan Hamblen <nathan@technically.us>
2012-04-26 08:30:10 -0400

    complete auth storage

--- src/main/scala/authorize.scala
+++ src/main/scala/authorize.scala
@@ -1,25 +1,39 @@
 package conscript
 
 import dispatch._
-import net.liftweb.json.JsonAST._
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
 
 object Authorize {
   def auths = :/("api.github.com").secure.POST / "authorizations"
 
+  // rm when avaialable in released dispatch
+  import com.ning.http.client.RequestBuilder
+  import com.ning.http.client.Realm.{RealmBuilder,AuthScheme}
+  def as_!(subject: RequestBuilder, user: String, password: String) =
+    subject.setRealm(new RealmBuilder()
+                     .setPrincipal(user)
+                     .setPassword(password)
+                     .setUsePreemptiveAuth(true)
+                     .setScheme(AuthScheme.BASIC)
+                     .build())
+
   import Conscript.http
   def apply(user: String, pass: String): Promise[Either[String, String]] =
     http(
-        auths.as_!(user, pass).setBody("{}") OK LiftJson.As
+        as_!(auths, user, pass).setBody(compact(render(
+          ("note" -> "Conscript") ~
+          ("note_url" -> "https://github.com/n8han/conscript")
+        ))) OK LiftJson.As
     ).either.left.map {
       case StatusCode(401) => "Unrecognized github login and password"
       case e => "Unexpected error: " + e.getMessage
-    }.right.flatMap {
-      case JField("token", JString(token)) =>
+    }.map { _.right.flatMap { js =>
+      (for (JField("token", JString(token)) <- js) yield {
         Config.properties {
           _.setProperty("gh.access", token)
         }
-        Promise(Right("Authorization stored"))
-      case _ =>
-        Promise(Left("JSON parsing error"))
-    }
+        "Authorization stored"
+      }).headOption.toRight("JSON parsing error")
+    } }
 }
