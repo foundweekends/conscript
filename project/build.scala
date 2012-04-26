c31ed9bc1c4dc4d70be283eb6bdb87c61b69eae3
Nathan Hamblen <nathan@technically.us>
2012-04-26 07:54:50 -0400

    preemptive basic auth

--- project/build.scala
+++ project/build.scala
@@ -3,9 +3,9 @@ import Keys._
 
 object ConscriptBuild extends Build {
   lazy val root =
-    Project("conscript", file(".")) /* dependsOn(dispatch)
+    Project("conscript", file(".")) dependsOn(dispatch)
   lazy val dispatch = ProjectRef(
     file("../reboot"),
     "core"
-  )*/
+  )
 }
--- src/main/scala/authorize.scala
+++ src/main/scala/authorize.scala
@@ -4,12 +4,12 @@ import dispatch._
 import net.liftweb.json.JsonAST._
 
 object Authorize {
-  val auths = :/("api.github.com").secure.POST / "authorizations"
+  def auths = :/("api.github.com").secure.POST / "authorizations"
 
   import Conscript.http
   def apply(user: String, pass: String): Promise[Either[String, String]] =
     http(
-        auths.as(user, pass).setBody("{}") OK LiftJson.As
+        auths.as_!(user, pass).setBody("{}") OK LiftJson.As
     ).either.left.map {
       case StatusCode(401) => "Unrecognized github login and password"
       case e => "Unexpected error: " + e.getMessage
