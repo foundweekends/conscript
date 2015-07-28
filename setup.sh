8bc594b2dd68925f07fbb5f838c58ad361db6003
MrOutis <mister.outis@hotmail.com>
2015-07-28 13:18:06 -0500

    Fix issues :no_mouth:

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-read -p "Type configuration directory (e.g. ~/.conscript): " CS
-read -p "Type installation directory (e.g.: ~/.bin): " BIN
+read -p "Type configuration directory (e.g. $HOME/.conscript): " CS
+read -p "Type installation directory (e.g. $HOME/.bin): " BIN
 
-CSCS=$CS/n8han/conscript/cs
-CLC=$CSCS/launchconfig
+CSCS="$CS/n8han/conscript/cs"
+CLC="$CSCS/launchconfig"
 
 mkdir -p $CSCS
-mkdir -p $BIN
+mkdir -p "$BIN"
 
 echo "Fetching current launch configuration..."
 wget https://raw.githubusercontent.com/n8han/conscript/master/src/main/conscript/cs/launchconfig -O $CLC
 
 echo "
 [boot]
-  directory: $CS/boot" >> $CLC
+  directory: $CS/boot" >> "$CLC"
 
 echo "#!/bin/sh
-java -jar $CS/sbt-launch.jar @$CLC \"\$@\"" > $BIN/cs
+java -jar $CS/sbt-launch.jar @$CLC \"\$@\"" > "$BIN/cs"
 
-chmod a+x $BIN/cs
+chmod a+x "$BIN/cs"
 
 LJV=0.13.8
 LJ="sbt-launch-$LJV.jar"
@@ -28,9 +28,7 @@ LJ="sbt-launch-$LJV.jar"
 if [ ! -f "$CS/$LJ" ]; then
     echo "Fetching launcher..."
     wget "https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/$LJV/sbt-launch.jar" -O "$CS/$LJ"
-    ln -sf $CS/$LJ $CS/sbt-launch.jar
+    ln -sf "$CS/$LJ" "$CS/sbt-launch.jar"
 fi
 
-echo "
-conscript installed to $BIN/cs
-"
+echo "conscript installed to $BIN/cs"
