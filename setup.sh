4efda1aeff42c35e56eaf488f7089c97064c3210
MrOutis <mister.outis@hotmail.com>
2015-07-28 12:46:30 -0500

    Change curl to wget and update launcher version to 0.13.8

--- setup.sh
+++ setup.sh
@@ -10,9 +10,8 @@ mkdir -p $CSCS
 mkdir -p $BIN
 
 echo "Fetching current launch configuration..."
+wget https://raw.githubusercontent.com/n8han/conscript/master/src/main/conscript/cs/launchconfig -O $CLC
 
-curl https://raw.githubusercontent.com/n8han/conscript/master/src/main/conscript/cs/launchconfig \
-    > $CLC
 echo "
 [boot]
   directory: $CS/boot" >> $CLC
@@ -22,14 +21,13 @@ java -jar $CS/sbt-launch.jar @$CLC \"\$@\"" > $BIN/cs
 
 chmod a+x $BIN/cs
 
-LJV=0.13.7
+LJV=0.13.8
 LJ="sbt-launch-$LJV.jar"
-if [ ! -f $CS/$LJ ]; then
-    echo "
-Fetching launcher...
-"
-    curl -L "http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/$LJV/sbt-launch.jar" \
-        > $CS/$LJ
+
+# If launcher is not in configuration directory
+if [ ! -f "$CS/$LJ" ]; then
+    echo "Fetching launcher..."
+    wget "https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/$LJV/sbt-launch.jar" -O "$CS/$LJ"
     ln -sf $CS/$LJ $CS/sbt-launch.jar
 fi
 
