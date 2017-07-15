3dbeca317c363ca4c224ba4d5f0f9eb44a64d1bf
xuwei-k <6b656e6a69@gmail.com>
2017-07-15 16:04:47 +0900

    avoid ssh-agent

--- publish-site.sh
+++ publish-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_REPO_SLUG}" == "foundweekends/conscript" ]]; then
-  eval "$(ssh-agent -s)" #start the ssh agent
+  echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy_rsa\n" >> ~/.ssh/config
   openssl aes-256-cbc -k "$SERVER_KEY" -in deploy_key.enc -d -a -out deploy_rsa
   chmod 600 deploy_rsa
-  ssh-add deploy_rsa
+  cp deploy_rsa ~/.ssh/
   sbt pushSiteIfChanged
 fi
