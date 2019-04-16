a73b6fc3a3db46badf88fc1937f0dc7c786fdb9f
kenji yoshida <6b656e6a69@gmail.com>
2019-04-17 08:49:55 +0900

    Update dependencies (#151)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,9 @@ import sbt._
 
 object Dependencies {
   val launcherInterface = "org.scala-sbt" % "launcher-interface" % "1.0.4"
-  val scalaSwing        = "org.scala-lang.modules" %% "scala-swing" % "2.1.0"
+  val scalaSwing        = "org.scala-lang.modules" %% "scala-swing" % "2.1.1"
   val dispatchCore      = "net.databinder.dispatch" %% "dispatch-core" % "0.11.2"
   val scopt             = "com.github.scopt" %% "scopt" % "3.7.1"
   val liftJson          = "net.liftweb" %% "lift-json" % "2.6.3"
-  val slf4jJdk14        = "org.slf4j" % "slf4j-jdk14" % "1.7.25"
+  val slf4jJdk14        = "org.slf4j" % "slf4j-jdk14" % "1.7.26"
 }
