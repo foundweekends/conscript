88a33a465122ed1b95b9c298994e5658485f206b
xuwei-k <6b656e6a69@gmail.com>
2023-05-14 14:23:08 +0900

    update sbt-laucnher version in setup scripts

--- setup.ps1
+++ setup.ps1
@@ -68,7 +68,7 @@ echo "
 echo "@echo off
 java %JAVA_OPTS% -jar $CS\sbt-launch.jar @file:\$CLC %*" | Out-File -Encoding "ASCII" "$BIN\cs.bat"
 
-$LJV = "1.3.3"
+$LJV = "1.4.2"
 $LJ = "launcher-$LJV.jar"
 
 # If launcher is not in configuration directory
