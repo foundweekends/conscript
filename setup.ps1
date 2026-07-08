169940746d47179bc2cee9a3353f6d8c1b65a341
xuwei-k <6b656e6a69@gmail.com>
2026-07-08 16:48:42 +0900

    Update launcher version in setup script

--- setup.ps1
+++ setup.ps1
@@ -68,7 +68,7 @@ echo "
 echo "@echo off
 java %JAVA_OPTS% -jar $CS\sbt-launch.jar @file:\$CLC %*" | Out-File -Encoding "ASCII" "$BIN\cs.bat"
 
-$LJV = "1.5.0"
+$LJV = "1.6.1"
 $LJ = "launcher-$LJV.jar"
 
 # If launcher is not in configuration directory
